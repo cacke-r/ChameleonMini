1467770bceb481c8a7e998bbf364b97375f49284
Federico Cerutti <federico@ceres-c.it>
2022-01-17 14:36:28 +0100

    Added missing shared ISRs signatures in header

--- Firmware/Chameleon-Mini/Codec/Codec.h
+++ Firmware/Chameleon-Mini/Codec/Codec.h
@@ -138,11 +138,12 @@ void isr_SniffISO14443_2A_CODEC_TIMER_LOADMOD_CCB_VECT(void);
 void isr_SNIFF_ISO15693_CODEC_TIMER_LOADMOD_CCB_VECT(void);
 extern void (* volatile isr_func_CODEC_TIMER_TIMESTAMPS_CCA_VECT)(void);
 void isr_Reader14443_2A_CODEC_TIMER_TIMESTAMPS_CCA_VECT(void);
-extern void (* volatile isr_func_CODEC_TIMER_TIMESTAMPS_CCB_VECT)(void);
-void isr_SniffISO14443_2A_CODEC_TIMER_TIMESTAMPS_CCB_VECT(void);
 void isr_SNIFF_ISO15693_CODEC_TIMER_TIMESTAMPS_CCA_VECT(void);
-extern void (* volatile isr_func_ACA_AC0_vect)(void); // TODO_sniff does this have to be shared? It is not used at this point
-extern void (* volatile isr_func_TCE0_CCA_vect)(void); // TODO_sniff does this have to be shared? It is not used at this point
+extern void (* volatile isr_func_CODEC_TIMER_TIMESTAMPS_CCB_VECT)(void); // TODO_sniff does this have to be shared? There is only one usage
+void isr_SniffISO14443_2A_CODEC_TIMER_TIMESTAMPS_CCB_VECT(void);
+extern void (* volatile isr_func_ACA_AC0_vect)(void);
+void isr_SniffISO14443_2A_ACA_AC0_VECT(void);
+void isr_SNIFF_ISO15693_ACA_AC0_VECT(void);
 
 INLINE void CodecInit(void) {
     ActiveConfiguration.CodecInitFunc();
